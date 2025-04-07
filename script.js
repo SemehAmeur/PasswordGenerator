b745bcf70b8d008b0e0455f62d82205960dc666f
SemehAmeur <semeh.ameur@gmail.com>
2025-04-07 23:28:27 +0100

    refaxtor(js): add options to generatePassword

--- script.js
+++ script.js
@@ -2,36 +2,56 @@ const generateBtn = document.querySelector(".generate-btn");
 const input = document.querySelector("input");
 const copyI = document.querySelector(".fa-copy");
 const alertContainer = document.querySelector(".alert-container");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
 
-const generatePassword = ()=>{
-    let key = "0123456789!@$%^&*()_+-{};:,.<>?/~`'";
-    for (let i = 65; i<=90; i++) {
-        key += String.fromCharCode(i);
-        key += (String.fromCharCode(i)).toLowerCase();
-    }
-    const passwordLength = 24;
-    let password = "";
-    for(let i=0; i<passwordLength; i++){
-        const randomNumber = Math.floor(Math.random() * key.length);
-        password += key.substring(randomNumber, randomNumber + 1)
-    }
-    input.value = password;
-}
+const generatePassword = () => {
+  let key = "";
+  let uppercase = "";
+  let lowercase = "";
+  const numbers = "0123456789";
+  const symbols = "!@$%^&*()_+-{};:,.<>?/~`'";
 
-const copyPassword = ()=>{
-    input.select();
-    input.setSelectionRange(0, 9999);
-    navigator.clipboard.writeText(input.value);
-}
+  for (let i = 65; i <= 90; i++) {
+    uppercase += String.fromCharCode(i);
+    lowercase += String.fromCharCode(i).toLowerCase();
+  }
 
-generateBtn.addEventListener("click", ()=>generatePassword())
+  if (uppercaseEl.checked) key += uppercase;
+  if (lowercaseEl.checked) key += lowercase;
+  if (numbersEl.checked) key += numbers;
+  if (symbolsEl.checked) key += symbols;
 
-copyI.addEventListener("click", ()=>{
-    copyPassword();
-    if(input.value){
-        alertContainer.classList.remove("active");
-        setTimeout(()=>{
-            alertContainer.classList.add("active")
-        }, 2000)
-    }
-})
+  if (key === "") {
+    alert("Please select at least one option");
+    return;
+  }
+
+  let password = "";
+  for (let i = 0; i < parseInt(lengthEl.value); i++) {
+    const randomNumber = Math.floor(Math.random() * key.length);
+    password += key.substring(randomNumber, randomNumber + 1);
+  }
+  input.value = password;
+};
+
+const copyPassword = () => {
+  input.select();
+  input.setSelectionRange(0, 9999);
+  navigator.clipboard.writeText(input.value);
+};
+
+generateBtn.addEventListener("click", () => generatePassword());
+
+copyI.addEventListener("click", () => {
+  copyPassword();
+  if (input.value) {
+    alertContainer.classList.remove("active");
+    setTimeout(() => {
+      alertContainer.classList.add("active");
+    }, 2000);
+  }
+});
